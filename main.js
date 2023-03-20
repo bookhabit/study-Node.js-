0ab5f7543c0d9a66493c9189dc762e934c3a9adb
bookhabit <qnrgoqlt@naver.com>
2023-03-21 03:01:57 +0900

    Nout Found출력과 home페이지 구현

--- main.js
+++ main.js
@@ -5,41 +5,67 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url,true).query;
-    var title = queryData.id
-    
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${_url ==='/' ? '환영합니다' : description}</p>
-        </body>
-        </html>
-        `;
-        response.end(template);
-    })
+    const pathname = url.parse(_url,true).pathname
 
-    
- 
+    if(pathname==="/"){
+        if(queryData.id === undefined){
+            // 홈페이지 - 쿼리에 id값이 없는경우
+            fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
+                var title= 'Welcome';
+                var description = 'Hello, Node.js'
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ol>
+                <h2>${title}</h2>
+                <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            })    
+        }else{
+            fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
+                var title = queryData.id
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ol>
+                <h2>${title}</h2>
+                <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            })
+        }
+        
+    }else{
+        response.writeHead(404);
+        response.end("Not Found");
+    }
+
 });
 app.listen(3000);
