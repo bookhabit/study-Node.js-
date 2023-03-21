b6c9c61d9f4d2a55dadc55124b53c9120ba6f144
bookhabit <qnrgoqlt@naver.com>
2023-03-21 18:59:06 +0900

    글 수정기능 완성

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
+const path = require('path');
 
 function templateHTML(title,list,body,controll) {
     return `
@@ -54,7 +55,14 @@ var app = http.createServer(function(request,response){
                 fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
                 var title = queryData.id
                 var list = templateList(fileList);
-                var template = templateHTML(title,list,`<h2>${title}</h2>${description}`,`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+                var template = templateHTML(title,list,`<h2>${title}</h2>${description}`,
+                `<a href="/create">create</a> 
+                <a href="/update?id=${title}">update</a>
+                <form action="delete_process" method="post" onsubmit="onSubmit">
+                    <input type="hidden" name="id" value="${title}">
+                    <input type="submit" value="delete">
+                </form>
+                `);
                 response.writeHead(200);
                 response.end(template);
                 })
@@ -67,7 +75,7 @@ var app = http.createServer(function(request,response){
                     var title= 'WEB - create';
                     var list = templateList(fileList);
                     var template = templateHTML(title,list,`
-                    <form action="http://localhost:3000/create_process" method="post">
+                    <form action="/create_process" method="post">
                         <p><input type="text" name="title" placeholder="title"></p>
                         <p>
                             <textarea name="description" placeholder="description"></textarea>
@@ -95,6 +103,49 @@ var app = http.createServer(function(request,response){
                 response.writeHead(302,{Location:`/?id=${title}`});
                 response.end(); 
             })
+        })
+    }
+    else if (pathname==="/update"){
+        fs.readdir("./data/",function(err,fileList){
+            fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
+            var title = queryData.id
+            var list = templateList(fileList);
+            var template = templateHTML(title,list,`
+            <form action="/update_process" method="post">
+                <input type="hidden" name="id" value="${title}"/>
+                <p><input type="text" name="title" placeholder="title" value=${title}></p>
+                <p>
+                    <textarea name="description" placeholder="description">${description}</textarea>
+                </p>
+                <p>
+                    <input type="submit"/>
+                </p>
+            </form>
+            `,
+            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+            response.writeHead(200);
+            response.end(template);
+            })
+        });
+    }else if (pathname==="/update_process"){
+        var body = "";
+        request.on('data',function(data){
+            body = body+data;
+        })
+        request.on('end',function(){
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+            // 파일 이름 수정
+            fs.rename(`data/${id}`,`data/${title}`,function(error){
+                // 파일 내용수정
+                fs.writeFile(`data/${title}`,description,'utf-8',function(err){
+                    response.writeHead(302,{Location:`/?id=${title}`});
+                    response.end(); 
+                })
+            })
+            
         })
     }
 
