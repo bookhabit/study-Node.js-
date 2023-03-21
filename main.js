35f2d159ab107be3fb1712a5b0769f92620d4113
bookhabit <qnrgoqlt@naver.com>
2023-03-21 15:53:14 +0900

    함수정의로 중복코드 없앰

--- main.js
+++ main.js
@@ -2,6 +2,34 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title,list,body) {
+    return `
+    <!doctype html>
+    <html>
+    <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+    </body>
+    </html>
+    `;
+}
+
+function templateList(fileList){
+    var list = `<ul>`;
+    var i =0 ;
+    while(i<fileList.length){
+        list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
+        i = i+1;
+    }
+    list = list+'</ul>';
+    return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url,true).query;
@@ -11,61 +39,20 @@ var app = http.createServer(function(request,response){
         if(queryData.id === undefined){
             // 홈페이지 - 쿼리에 id값이 없는경우
             fs.readdir("./data/",function(err,fileList){
-                console.log(fileList)
                 var title= 'Welcome';
                 var description = 'Hello, Node.js'
-                var list = `<ul>`;
-                var i =0 ;
-                while(i<fileList.length){
-                    list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
-                    i = i+1;
-                }
-                list = list+'</ul>';
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                <h2>${title}</h2>
-                <p>${description}</p>
-                </body>
-                </html>
-                `;
+                var list = templateList(fileList);
+                
+                var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
                 response.writeHead(200);
                 response.end(template); 
             })
         }else{
             fs.readdir("./data/",function(err,fileList){
-                console.log(fileList)
-                var list = `<ul>`;
-                var i =0 ;
-                while(i<fileList.length){
-                    list = list + `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`
-                    i = i+1;
-                }
-                list = list+'</ul>';
-            fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
+                fs.readFile(`data/${queryData.id}`,'utf-8',function(err,description){
                 var title = queryData.id
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                <h2>${title}</h2>
-                <p>${description}</p>
-                </body>
-                </html>
-                `;
+                var list = templateList(fileList);
+                var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
                 response.writeHead(200);
                 response.end(template);
             })
