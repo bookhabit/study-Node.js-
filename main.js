c35dd806af0886f3b3dc67740ac6cdcf459a6c08
bookhabit <qnrgoqlt@naver.com>
2023-03-21 18:08:33 +0900

    글생성과 리다이렉션

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title,list,body) {
     return `
@@ -13,6 +14,7 @@ function templateHTML(title,list,body) {
     <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
     </body>
     </html>
@@ -34,7 +36,7 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url,true).query;
     const pathname = url.parse(_url,true).pathname
-
+    console.log(pathname)
     if(pathname==="/"){
         if(queryData.id === undefined){
             // 홈페이지 - 쿼리에 id값이 없는경우
@@ -42,7 +44,6 @@ var app = http.createServer(function(request,response){
                 var title= '-Welcome-';
                 var description = 'Hello, Node.js'
                 var list = templateList(fileList);
-                
                 var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
                 response.writeHead(200);
                 response.end(template); 
@@ -55,11 +56,48 @@ var app = http.createServer(function(request,response){
                 var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
                 response.writeHead(200);
                 response.end(template);
+                })
+            });
+        }       
+    }else if (pathname==="/create"){
+            if(queryData.id === undefined){
+                // 홈페이지 - 쿼리에 id값이 없는경우
+                fs.readdir("./data/",function(err,fileList){
+                    var title= 'WEB - create';
+                    var list = templateList(fileList);
+                    var template = templateHTML(title,list,`
+                    <form action="http://localhost:3000/create_process" method="post">
+                        <p><input type="text" name="title" placeholder="title"></p>
+                        <p>
+                            <textarea name="description" placeholder="description"></textarea>
+                        </p>
+                        <p>
+                            <input type="submit"/>
+                        </p>
+                    </form>
+                    `);
+                    response.writeHead(200);
+                    response.end(template); 
+                })
+            }
+    }else if(pathname==="/create_process") {
+        var body = "";
+        request.on('data',function(data){
+            body = body+data;
+        })
+        request.on('end',function(){
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            console.log(post.title)
+            fs.writeFile(`data/${title}`,description,'utf-8',function(err){
+                response.writeHead(302,{Location:`/?id=${title}`});
+                response.end(); 
             })
-        });
-        }
-        
-    }else{
+        })
+    }
+
+else{
         response.writeHead(404);
         response.end("Not Found");
     }
