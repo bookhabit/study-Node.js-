54da0e2cd787158c1e454d2d0388a59b25697dcb
bookhabit <qnrgoqlt@naver.com>
2023-03-22 11:56:21 +0900

    mysql 연동해서 홈페이지 구현

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var mysql = require('mysql');
+
+var db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '2719',
+  database : 'opentutorials'
+});
+ 
+db.connect(); // 데이터베이스 연결
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -12,14 +22,26 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        fs.readdir('./data', function(error, filelist){
+        // fs.readdir('./data', function(error, filelist){
+          // var title = 'Welcome';
+          // var description = 'Hello, Node.js';
+          // var list = template.list(filelist);
+        //   var html = template.HTML(title, list,
+        //     `<h2>${title}</h2>${description}`,
+        //     `<a href="/create">create</a>`
+        //   );
+        //   response.writeHead(200);
+        //   response.end(html);
+        // });
+        db.query('SELECT * FROM topic', function (error, topics) {
           var title = 'Welcome';
           var description = 'Hello, Node.js';
-          var list = template.list(filelist);
+          var list = template.list(topics);
           var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
+          `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`
           );
+          
           response.writeHead(200);
           response.end(html);
         });
