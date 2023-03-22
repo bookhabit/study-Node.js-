85723096f0c3be2957dcb0ca2f5a84581bd048f1
bookhabit <qnrgoqlt@naver.com>
2023-03-22 15:46:06 +0900

    join 이용한 글 생성 구현

--- main.js
+++ main.js
@@ -74,25 +74,37 @@ var app = http.createServer(function(request,response){
     } else if(pathname === '/create'){ 
       // 글 생성 페이지 - 클라이언트 화면
       db.query(`SELECT * FROM topic`, function (error, topics) {
-        var title = 'Create';
-        var list = template.list(topics);
-        var html = template.HTML(title, list,
-        `
-        <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-        </form>
-        `,
-        `<a href="/create">create</a>`
-        );
-        
-        response.writeHead(200);
-        response.end(html);
+        db.query(`SELECT * FROM author`,function(error2,authors){
+          var tag ='';
+          var i=0;
+          while(i<authors.length){
+            tag +=`<option value=${authors[i].id}>${authors[i].name}</option>`;
+            i++;
+          }
+
+          var title = 'Create';
+          var list = template.list(topics);
+          var html = template.HTML(title, list,
+          `
+          <form action="/create_process" method="post">
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p>
+                <textarea name="description" placeholder="description"></textarea>
+              </p>
+              <p>
+                ${template.authorSelect(authors)}
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+          </form>
+          `,
+          `<a href="/create">create</a>`
+          );
+          
+          response.writeHead(200);
+          response.end(html);
+        })
       });
     } else if(pathname === '/create_process'){
       // 글 생성 페이지 - 서버 측 로직
@@ -105,7 +117,7 @@ var app = http.createServer(function(request,response){
         db.query(`
           INSERT INTO topic (title, description, created, author_id) 
             VALUES(?, ?, NOW(), ?)`,
-          [post.title, post.description, 1], 
+          [post.title, post.description, post.author], 
           function(error, result){
             if(error){
               throw error;
